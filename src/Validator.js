71c178c29722faaaecd38272ace17b1635551514
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-10-30 13:53:37 +0300

    step 3

--- src/Validator.js
+++ src/Validator.js
@@ -1,9 +1,15 @@
 import NumberSchema from './NumberSchema.js';
+import ArraySchema from './ArraySchema.js';
 
 export default class Validator {
   number() {
     const validator = (value) => typeof value === 'number';
     return new NumberSchema([validator]);
+  }
+
+  array() {
+    const validator = (value) => Array.isArray(value);
+    return new ArraySchema([validator]);
   }
 
 }
